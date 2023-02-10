9daca1e7cbda5123a53486766fbbcd435929b3a9
Soumil Tewari <75936954+Soumil-Tewari@users.noreply.github.com>
2023-02-10 15:31:36 +0530

    Delete cs.py

--- cs.py
+++
@@ -1,6 +0,0 @@
-numberList=[int(input("Enter the first number")),
-int(input("Enter the second number")),
-int(input("Enter the third number"))]
-print("Largest number: ",max(numberList))
-print("Smallest number: ",min(numberList))
-
